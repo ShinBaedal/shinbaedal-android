7a28488bf6c0bd762c49c639f5424947f1a42c2b
ckrudals <2004ckck@naver.com>
2021-11-25 03:49:15 +0900

    [UPDATE] BaseFragment 사용

--- Hackathon/app/src/main/java/com/example/hackathon/view/fragment/OwnerMainFragment.kt
+++ Hackathon/app/src/main/java/com/example/hackathon/view/fragment/OwnerMainFragment.kt
@@ -1,33 +1,23 @@
 package com.example.hackathon.view.fragment
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.hackathon.R
+import com.example.hackathon.base.BaseFragment
+import com.example.hackathon.databinding.OwnerMainFragmentBinding
 import com.example.hackathon.viewmodel.OwnerMainViewModel
 
-class OwnerMainFragment : Fragment() {
+class OwnerMainFragment : BaseFragment<OwnerMainFragmentBinding>(R.layout.owner_main_fragment) {
 
-    companion object {
-        fun newInstance() = OwnerMainFragment()
+    private val viewModel: OwnerMainViewModel by viewModels()
+
+    override fun OwnerMainFragmentBinding.onViewCreated() {
     }
 
-    private lateinit var viewModel: OwnerMainViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.owner_main_fragment, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(OwnerMainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
