8e6c8098b5e4c14f51727d6a083d48b998e2e9a5
ckrudals <2004ckck@naver.com>
2021-11-25 11:54:18 +0900

    [CREATE] BaseDialogFragment

---
+++ Hackathon/app/src/main/java/com/example/hackathon/base/BaseDialogFragment.kt
@@ -0,0 +1,29 @@
+package com.example.hackathon.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.DialogFragment
+
+open class BaseDialogFragment<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) : DialogFragment() {
+
+    lateinit var binding: T
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        binding.onCreateView()
+        binding.onViewCreated()
+        return binding.root
+    }
+
+    open fun T.onCreateView() = Unit
+    open fun T.onViewCreated() = Unit
+}
