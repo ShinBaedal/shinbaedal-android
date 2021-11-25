fdc1de476ab04ec46087517c2a8f21536c8ea623
ckrudals <2004ckck@naver.com>
2021-11-25 11:52:47 +0900

    [CREATE] OwnerStoreAddFragment 생성

---
+++ Hackathon/app/src/main/java/com/example/hackathon/view/fragment/OwnerStoreAddFragment.kt
@@ -0,0 +1,26 @@
+package com.example.hackathon.view.fragment
+
+import com.example.hackathon.R
+import com.example.hackathon.base.BaseFragment
+import com.example.hackathon.databinding.OwnerStoreAddFragmentBinding
+import com.example.hackathon.view.dialog.OwnerStoreAddBottomSheetFragment
+
+class OwnerStoreAddFragment :
+    BaseFragment<OwnerStoreAddFragmentBinding>(R.layout.owner_store_add_fragment) {
+    override fun OwnerStoreAddFragmentBinding.onCreateView() {
+
+    }
+
+    override fun OwnerStoreAddFragmentBinding.onViewCreated() {
+        addButtonClick()
+    }
+
+
+
+    private fun addButtonClick() {
+        val dialog = OwnerStoreAddBottomSheetFragment()
+        binding.addBtn.setOnClickListener {
+            dialog.show(parentFragmentManager, "dialog")
+        }
+    }
+}
