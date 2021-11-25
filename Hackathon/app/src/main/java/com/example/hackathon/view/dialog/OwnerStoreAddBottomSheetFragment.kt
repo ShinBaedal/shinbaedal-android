cd54ba94647485c78ff2b380d7ce46e56231e645
ckrudals <2004ckck@naver.com>
2021-11-25 11:51:39 +0900

    [CREATE] AddStoreBottmSheet dialog 생성

---
+++ Hackathon/app/src/main/java/com/example/hackathon/view/dialog/OwnerStoreAddBottomSheetFragment.kt
@@ -0,0 +1,21 @@
+package com.example.hackathon.view.dialog
+
+import com.example.hackathon.R
+import com.example.hackathon.base.BaseDialogFragment
+import com.example.hackathon.databinding.OwnerStoreAddBottomSheetDialogBinding
+
+class OwnerStoreAddBottomSheetFragment :
+    BaseDialogFragment<OwnerStoreAddBottomSheetDialogBinding>(R.layout.owner_store_add_bottom_sheet_dialog) {
+
+    override fun OwnerStoreAddBottomSheetDialogBinding.onCreateView() {
+
+    }
+
+    override fun OwnerStoreAddBottomSheetDialogBinding.onViewCreated() {
+
+    }
+
+    private fun sendData(){
+
+    }
+}
